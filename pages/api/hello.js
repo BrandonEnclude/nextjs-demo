cebb90fb89afc246ba99a113b0203fcd121fcf64
brandon.dematteis@enclude.ie <brandon.dematteis@enclude.ie>
2021-01-13 08:46:33 +0000

    first commit

---
+++ pages/api/hello.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+    const {
+        query: { name },
+    } = req
+    res.status(200).json({'message': `Hello ${name}`})
+}
--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,9 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          WELCOME to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h2>This is the third deployment</h2>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
