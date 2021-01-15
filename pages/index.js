ea4353d2bd9e5c5ac9a91faef8e386ab685cddcf
brandon.dematteis@enclude.ie <brandon.dematteis@enclude.ie>
2021-01-15 17:21:09 +0000

    redirectUri fix

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
   const clientId = '3MVG95G8WxiwV5Ptib1Zg_oPWxuJAgNilF16FmayvE8pOKjifuU76CwnwjBI0zpFGSFP2c6KmmOTfJtjBulGs'
-  const redirectUri = 'http://localhost:3000/callback'
+  const redirectUri = 'https://enc-demo.azurewebsites.net/callback'
   const loginUrl = `https://bd1887-developer-edition.eu32.force.com/nextdemo/services/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`
   
   return (
